90e4c514947b75c2a3c1c93c8a690ef33c70e0b9
Vitalii Pavliuk <pavliuk96@gmail.com>
2022-09-23 12:05:20 +0300

    prettify edit

--- src/components/TaskActions.tsx
+++ src/components/TaskActions.tsx
@@ -1,33 +1,27 @@
-import React, {useCallback, useState} from 'react';
+import React, {useCallback} from 'react';
 
 import EditIcon from '@mui/icons-material/Edit';
 import Fab from '@mui/material/Fab';
 
-import TaskEditPopup from './TaskEditPopup';
 import TaskActionsProps from './TaskActionsProps';
+import {useDispatch} from "../store";
+import {taskEditInit} from "../reducers/task";
 
 
 const TaskActions = (props: TaskActionsProps) => {
-
     const {task} = props;
 
-    const [open, setOpen] = useState(false);
-
+    const dispatch = useDispatch();
 
     const onEditClick = useCallback(() => {
-        setOpen(true);
-    }, []);
-
-    const onClose = useCallback(() => {
-        setOpen(false);
-    }, []);
+        dispatch(taskEditInit(task))
+    }, [dispatch, task]);
 
     return (
         <>
             <Fab color="secondary" aria-label="edit">
                 <EditIcon onClick={onEditClick}/>
             </Fab>
-            <TaskEditPopup open={open} task={task} onClose={onClose}/>
         </>
     );
 
--- src/reducers/tasks.ts
+++ src/reducers/tasks.ts
@@ -1,6 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit';
+import type {PayloadAction} from '@reduxjs/toolkit';
+import {createSlice} from '@reduxjs/toolkit';
 import Task from '../data/Task';
-import type { PayloadAction } from '@reduxjs/toolkit';
 
 interface TasksState {
     tasks: Task[];
@@ -17,26 +17,19 @@ export const slice = createSlice({
   reducers: {
 
     setTasks: (state : TasksState, action: PayloadAction<Task[]>) => {
-
         state.tasks = action.payload;
-
     },
 
-    updateTaskName: (state : TasksState, action: PayloadAction<[number, string]>) => { 
+    updateTask: (state : TasksState, action: PayloadAction<Task>) => {
+        const updatedTask = action.payload;
 
-        console.log("Update task name");
-        
-        const [taskId, newTaskName] = action.payload;
+        const index = state.tasks.findIndex(task => task.id === updatedTask.id);
 
-        const index = state.tasks.findIndex(updatedTask => updatedTask.id === taskId); 
-
-        const task = state.tasks[index]!;
-
-        const newTask = {...task, name: newTaskName};
-        
-        const newArray = [...state.tasks.slice(0, index), newTask, ...state.tasks.slice(index + 1)]; 
-
-        state.tasks = newArray;
+        state.tasks = [
+            ...state.tasks.slice(0, index),
+            updatedTask,
+            ...state.tasks.slice(index + 1)
+        ];
 
     },
 
@@ -51,6 +44,6 @@ export const slice = createSlice({
 
 export const { reducer } = slice;
 
-export const { setTasks, updateTaskName, fetchTasks} = slice.actions;
+export const { setTasks, updateTask } = slice.actions;
 
 export default slice.reducer;
